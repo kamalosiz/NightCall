fb619e2fbe96a6c80ccb6c3afdc35063398d709e
Ali.Raza <ali.raza@softech.com.pk>
2019-10-24 17:23:02 +0500

    make reset password screen

---
+++ app/src/main/java/com/example/kalam_android/view/activities/ResetPasswordActivity.kt
@@ -0,0 +1,16 @@
+package com.example.kalam_android.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.kalam_android.R
+import com.example.kalam_android.databinding.ActivityResetPasswordBinding
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityResetPasswordBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_reset_password)
+    }
+}
