9c88e28a5c147687bda739facc7a075ea97489bb
Ali.Raza <ali.raza@softech.com.pk>
2020-01-28 15:37:12 +0500

    My Final Code

--- app/src/main/java/com/example/kalam_android/view/activities/AttachmentActivity.kt
+++ app/src/main/java/com/example/kalam_android/view/activities/AttachmentActivity.kt
@@ -184,7 +184,7 @@ class AttachmentActivity : AppCompatActivity(), MyClickListener, OnStartDragList
                     )
                 }
                 1 -> {
-                    
+
                 }
             }
         }
